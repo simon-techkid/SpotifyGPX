96ed932e80d15b158ce14722f1a75e3a5bb5bfb1
TheBoyLeastLikelyTo <35753008+TheBoyLeastLikelyTo@users.noreply.github.com>
2023-10-04 11:46:24 -0700

    SpotifyGPX 2.4.3

--- Program.cs
+++ Program.cs
@@ -240,9 +240,26 @@ class JSON
                 JObject songEntry = new()
                 {
                     ["ts"] = entry.Time_End.ToString(spotifyMiniJson ? "yyyy-MM-dd HH:mm" : "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture),
-                    ["master_metadata_album_artist_name"] = entry.Song_Artist,
+                    ["username"] = entry.Spotify_Username,
+                    ["platform"] = entry.Spotify_Platform,
+                    ["ms_played"] = entry.Time_Played,
+                    ["conn_country"] = entry.Spotify_Country,
+                    ["ip_addr_decrypted"] = entry.Spotify_IP,
+                    ["user_agent_decrypted"] = entry.Spotify_UA,
                     ["master_metadata_track_name"] = entry.Song_Name,
-                    ["ms_played"] = entry.Time_Played
+                    ["master_metadata_album_artist_name"] = entry.Song_Artist,
+                    ["master_metadata_album_album_name"] = entry.Song_Album,
+                    ["spotify_track_uri"] = entry.Episode_URI,
+                    ["episode_name"] = entry.Episode_Name,
+                    ["episode_show_name"] = entry.Episode_Show,
+                    ["spotify_episode_uri"] = entry.Episode_URI,
+                    ["reason_start"] = entry.Song_StartReason,
+                    ["reason_end"] = entry.Song_EndReason,
+                    ["shuffle"] = entry.Song_Shuffle,
+                    ["skipped"] = entry.Song_Skipped,
+                    ["offline"] = entry.Spotify_Offline,
+                    ["offline_timestamp"] = entry.Spotify_OfflineTS,
+                    ["incognito"] = entry.Spotify_Incognito
                 };
 
                 json.Add(songEntry);
