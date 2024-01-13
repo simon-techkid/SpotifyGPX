7969e513c44b5dd387250c8a441536682c5a5f9e
TheBoyLeastLikelyTo <35753008+TheBoyLeastLikelyTo@users.noreply.github.com>
2024-01-12 23:37:47 -0800

    2024.8.1
    - Bug fix where overlapping (spanning the same time period) or duplicate GPX tracks fielded a duplicate set of SpotifyEntries

--- SpotifyGPX/JsonFile.cs
+++ SpotifyGPX/JsonFile.cs
@@ -2,6 +2,7 @@
 
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -16,17 +17,34 @@ public readonly struct JsonFile
 
     public JsonFile(string path) => jsonFilePath = path;
 
-    public readonly List<SpotifyEntry> FilterSpotifyJson(List<GPXTrack> gpxPoints)
+    public readonly List<SpotifyEntry> FilterSpotifyJson(List<GPXTrack> tracks)
     {
+        Dictionary<int, (DateTimeOffset startTime, DateTimeOffset endTime)> trackRange = TrackRanges(tracks);
         List<SpotifyEntry> allSongs = JsonContents.Select((json, index) => new SpotifyEntry(index, json)).ToList();
 
-        return gpxPoints.SelectMany(track =>
-        {
-            // Filter Spotify entries based on track-specific start and end times
-            return allSongs
-            .Where(entry => (entry.Time >= track.Start) && (entry.Time <= track.End)) // Song played between start & end of the GPX
-            .ToList(); // Send all the relevant songs to a list!
-        })
-        .ToList();
+        // Filter Spotify entries based on track-specific start and end times
+        return allSongs
+        .Where(entry => trackRange.Any(trackTimes => // Return true if the song falls inside the GPX track
+            (entry.Time >= trackTimes.Value.startTime) && (entry.Time <= trackTimes.Value.endTime))) // Song played between start & end of the GPX
+        .ToList(); // Send all the relevant songs to a list!
     }
+
+    public readonly Dictionary<int, (DateTimeOffset startTime, DateTimeOffset endTime)> TrackRanges(List<GPXTrack> gpxTracks)
+    {
+        // Get start and end times for each GPX track
+        return gpxTracks // Returns track number, start time of the track, and end time of the track
+            .SelectMany(track => track.Points) // Flatten List<GPXTrack> to List<GPXPoint>
+            .GroupBy(point => point.TrackIndex) // Distinguish which track each point came from
+            .ToDictionary(
+                group => group.Key, // Dictionary key is the track member
+                group => group.Aggregate(
+                    (startTime: group.First().Time, endTime: group.First().Time),
+                    (earliest, point) => (
+                        startTime: point.Time < earliest.startTime ? point.Time : earliest.startTime,
+                        endTime: point.Time > earliest.endTime ? point.Time : earliest.endTime
+                    )
+                )
+            );
+    }
+
 }
