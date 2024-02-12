b3e7338fdffbd85db2dbbe76c3602c3399d32ff7
TheBoyLeastLikelyTo <35753008+TheBoyLeastLikelyTo@users.noreply.github.com>
2024-02-11 16:34:22 -0800

    2024.23.3
    - Nullability in Output.Txt fixed

--- SpotifyGPX/Output/Txt.cs
+++ SpotifyGPX/Output/Txt.cs
@@ -8,16 +8,16 @@ public class Txt : IFileOutput
 {
     public Txt(IEnumerable<SongPoint> pairs) => Document = GetDocument(pairs);
 
-    private string[] Document { get; }
+    private string?[] Document { get; }
 
-    private static string[] GetDocument(IEnumerable<SongPoint> Pairs)
+    private static string?[] GetDocument(IEnumerable<SongPoint> Pairs)
     {
-        return Pairs.Select(pair => pair.Song.Song_URI).Where(s => s != null).ToArray();
+        return Pairs.Select(pair => pair.Song.Song_URI).ToArray();
     }
 
     public void Save(string path)
     {
-        File.WriteAllLines(path, Document);
+        File.WriteAllLines(path, Document.Where(uri => uri != null)!); // Ensure no empty/null URI lines are created
     }
 
     public int Count => Document.Length;
